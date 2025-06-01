1c897d2259ca1c1ffec2cd3b7e74735c13af493f
Bagas Gilang Ramadhan <bagasiayin205@gmail.com>
2025-06-01 23:17:05 +0700

    update prt

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9000;
 const SECRET_KEY = 'segarikan-secret-key';
 
 // CORS
